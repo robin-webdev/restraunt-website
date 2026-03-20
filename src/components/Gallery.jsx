6c4d30578e7e0db7d4b61cd334bd7352e7a32044
robin-webdev <robin.webdev.me@gmail.com>
2026-03-20 10:00:07 +0200

    dd'

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const Gallery = () => {
   return (
     <section id="gallery" className="flex flex-col gap-10 font-poppins scroll-mt-18">
-      <h1 className="  stroke-[#d2e40f] flex flex-col items-center text-white text-poppins gap-1 text-3xl font-bold text-center lg:text-4xl">
+      <h1 className="  stroke-[#d2e40f] flex flex-col items-center text-(--theme-text-primary) text-poppins gap-1 text-3xl font-bold text-center lg:text-4xl">
         Our Gallery
         <div className="h-1 w-30 rounded-2xl bg-[#d2e40f]" />
       </h1>
 
       <div className="flex gap-10 p-4 overflow-auto w-screen">
         <img
-          src="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSwUJO15RkkyeReR4bGQJNpDtGo6uRt8xPFQ9oVSXG24l9i2uJ0GIuY-pICMRD7eEVvMD2NNXyYzbdWU4cYVDAHFRzSKqg2dXiDQeqAHBLxHeZTIwN09f2VJtlYP2u15gwH-4Blak-z4xZvr=s1360-w1360-h1020-rw"
+          src="https://lh3.googleusercontent.com/gps-cs-s/AHVAwerOxShWu4OxE0PIWp2Jkm1DgI3Tp-xvsaW7J0_yrFlaB88MDPsaO_LCBoRBReW0qhku81RijevdKssCjVfFaCWeI0rINTBT07gJx-NilTZYA8A8rcWyFzL6YP7YncJLLuXE3g1LEpzsqVy9=s846-k-no"
           className="rounded-3xl h-100 min-w-70 sm:min-w-90 sm:h-120  object-cover shadow-lg/30 shadow-[#c6c6c6]"
           alt=""
         />
@@ -21,22 +21,22 @@ const Gallery = () => {
         />
         <img
           className="rounded-3xl h-100 min-w-70 sm:min-w-90 sm:h-120  object-cover shadow-lg/30 shadow-[#c6c6c6]"
-          src="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSyb5MurZ5x9tKAIqqPF189gw67B8XkITLrFr69DDz0rwAJyT48QdrBMIaZHNjn533ei8I1v9CiRStEGDIY4IQ1zZUAq9BeDFwy63fXPwXmnffZqnP8u0aAqrEzELAcap3fQ3Ma1_yIRTQY=s1360-w1360-h1020-rw"
+          src="https://lh3.googleusercontent.com/gps-cs-s/AHVAwepK0LLaijJIf9DKi9Ev1FBE9PbUupgdaODG2_legb0xXKn7xn4KNADLHkHQjkK4GMwgwzzHDNvHhTbJofmFv2cftkWlT0fOSVbZFIJmkodKjYA6GMJYzyE-PAI7u-iARbasYgdc7mFIQprq=s483-k-no"
           alt=""
         />
         <img
           className="rounded-3xl h-100 min-w-70 sm:min-w-90 sm:h-120  object-cover shadow-lg/30 shadow-[#c6c6c6]"
-          src="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSwHawFO1YnQLXoQ51cBUbDwKUHRuTfKDsKIVz7hpNEGW7z_MIY8Q53qgP7X64t8W6AhDC3KQkcIcjxwaWACyfxSdo4dI3FNgBqy97VQ7on4aBo5BMXGyQmHPBplIyOrh3J1BGkZ=s1360-w1360-h1020-rw"
+          src="https://lh3.googleusercontent.com/gps-cs-s/AHVAweoq04NNyXlXfDDhDhJCrdjYNhTfnc3LvuB65Gr76os8AUG2C29jk12rfDSc7AfynOFMnYlghbZ3UaKzRCh9tM8PTRFtoCDaDDUlsoPjkLcRaVSzs-tfZv2x28UNAkelXDuguSgRKITEwME=s661-k-no"
           alt=""
         />
         <img
           className="rounded-3xl h-100 min-w-70 sm:min-w-90 sm:h-120  object-cover shadow-lg/30 shadow-[#c6c6c6]"
-          src="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSzMFRUFNLBarGD9x0FDKmIDbAgEjLNjlFJu3cqjPYIT3mnnRZTs9xSwRSIeYzRmEhT7w4cYOlZ7EcGb2sWLEGutJM5SfKWay_DNj77YQMXX64ty_ysoz_3UtvARQPojumIhJ96AzQ2Pg2Q=s1360-w1360-h1020-rw"
+          src="https://lh3.googleusercontent.com/gps-cs-s/AHVAwepJJ-7qGhVEol_6JEL-g93Fw3M1sAx2jNS1SnMWtgXTGAd7FZCGM97J7CBUgRs3WZ_E8LQXnAuVFfz-hwjdCzxGF-aE8LnFGOltO7F71K8uRk-kbhb1kTsvKNB50p8e9zVZ5bCE_uYyTkqk=s575-k-no"
           alt=""
         />
         <img
           className="rounded-3xl h-100 min-w-70 sm:min-w-90 sm:h-120  object-cover shadow-lg/30 shadow-[#c6c6c6]"
-          src="https://lh3.googleusercontent.com/gps-cs-s/AG0ilSy-gH0k7qOnXgriS2R3lTXa1CkM8t5HT08z1c9cvFpKp7N-qcfiX8ng7q0u76EsHvso92kdAvAmhg6OZrF7kqgK9SdVh71rC31ajX2PoPWC4MV1Mf551Nwh3onb4vsAU14HwhANfmgZ924O=s1360-w1360-h1020-rw"
+          src="https://lh3.googleusercontent.com/gps-cs-s/AHVAweouIIo6BF2qEzoYzYi_6il38Alce9sjeTYeRROfahvQJbOkKoqBZ_NLEks0f6xskl4lpzfHiriPt6PFb8701B9-ci-tby6GCPK8uxzhaXHYwLxPE9ll9BWbJmgetInjpY4o-kfsR0N3ieZZ=s508-k-no"
           alt=""
         />
       </div>
